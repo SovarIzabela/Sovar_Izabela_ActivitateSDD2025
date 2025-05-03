c8fb9b43301afb8a671e6526a622c7173fa698e4
sovarizabela22@stud.ase.ro <sovarizabela22@stud.ase.ro>
2025-05-03 20:36:54 +0300

    commit

--- ListaDublaFarmacie.c
+++ ListaDublaFarmacie.c
@@ -1,227 +1,325 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//trebuie sa folositi fisierul masini.txt
-//sau va creati un alt fisier cu alte date
-
-struct Farmacie {
-
-	int id;
-	char* nume;
-	unsigned char numarMedicamente;
-	float* preturiMedicamente;
-
-};
-typedef struct Farmacie Farmacie;
-
-
-struct Nod {
-
-	Farmacie info;
-	struct Nod* prev;
-	struct Nod* next;
-
-};
-typedef struct Nod Nod;
-
-
-
-struct ListaDubla {
-	Nod* first;
-	Nod* last;
-	int nrNoduri;
-};
-typedef struct ListaDubla ListaDubla;
-
-Farmacie citireFarmacieDinFisier(FILE* file) {
-
-	Farmacie f = { 0,NULL,0,NULL };
-	char buffer[100];
-	char* separator = ",;\n";
-	fgets(buffer, 100, file);
-
-	
-	char* token = strtok(buffer, separator);
-	if (token == NULL) return f;
-	f.id = atoi(token);
-
-	 token = strtok(NULL, separator);
-	if (token== NULL) return f;
-	f.nume = (char*)malloc(strlen(token) + 1);
-	strcpy(f.nume, token);
-
-	token = strtok(NULL, separator);
-	if (token == NULL) return f;
-	f.numarMedicamente = (unsigned char)atoi(token);
-
-	token = strtok(NULL, separator);
-	
-	f.preturiMedicamente = (float*)malloc(sizeof(float) * f.numarMedicamente);
-	if (f.preturiMedicamente == NULL) {
-		free(f.nume);
-		return f;
-	}
-	for (int i = 0; i < f.numarMedicamente; i++) {
-		 token = strtok(NULL, separator);
-		 if (token == NULL) {
-			 free(f.nume);
-			 free(f.preturiMedicamente);
-			 f.nume = NULL;
-			 f.preturiMedicamente = NULL;
-			 return f;
-		 }
-		 f.preturiMedicamente[i] = atof(token);
-	}
-
-	return f;
-
-}
-
-void afisareFarmacii(Farmacie farmacie) {
-
-	printf("ID este : %d\n", farmacie.id);
-	printf("Numele farmaciei este : %s\n", farmacie.nume);
-	printf("Numar de medicamente : %d\n", farmacie.numarMedicamente);
-	printf("Preturile sunt : %.2f\n", farmacie.preturiMedicamente);
-	printf("\n");
-	
-}
-
-void afisareListaFarmaciiINCEPUT(ListaDubla lista) {
-	Farmacie f;
-	Nod* p = lista.first;
-	while (p) {
-
-		afisareFarmacii(p->info);
-		p = p->next;
-	}
-
-
-}
-
-void afisareListaFarmaciiSfarsit(ListaDubla lista) {
-	Farmacie f;
-	Nod* p = lista.last;
-	while (p) {
-
-		afisareFarmacii(p->info);
-		p = p->prev;
-	}
-
-
-}
-
-void adaugaFarmacieInListaFINAL(ListaDubla*lista, Farmacie FarmacieNoua) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->info = FarmacieNoua;
-	nou->next = NULL;
-	nou->prev = lista->last;
-
-	if (lista->last = NULL) {
-		lista->last->next = nou;
-	}
-	else {
-		lista->first = nou;
-	}
-	lista->last = nou;
-	lista->nrNoduri++;
-
-}
-
-void adaugaLaInceputInLista(ListaDubla* lista, Farmacie FarmacieNoua) {
-	Nod* nou = (Nod*)malloc(sizeof(Nod));
-	nou->info = FarmacieNoua;
-	nou->next = NULL;
-	nou->prev = lista->first;
-
-	if (lista->first == NULL) {
-		lista->first->prev = nou;
-	}
-	else {
-		lista->last = nou;
-	}
-
-	lista->first = nou;
-	lista->nrNoduri++;
-
-}
-
-ListaDubla citireLDFarmacieDinFisier(const char* numeFisier) {
-	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
-	//prin apelul repetat al functiei citireMasinaDinFisier()
-	//ATENTIE - la final inchidem fisierul/stream-ul
-	FILE* f = fopen(numeFisier, "r");
-	ListaDubla lista;
-	lista.first = NULL;
-	lista.last = NULL;
-	lista.nrNoduri = 0;
-
-	while (!feof(f)) {
-		adaugaLaInceputInLista(&lista, citireFarmacieDinFisier(f));
-
-	}
-
-
-	fclose(f);
-	return lista;
-}
-
-void dezalocareLDFarmacie(ListaDubla* lista) {
-	Nod* p = lista->first;
-	while (p) {
-		Nod* aux = p;
-		p = p->next;
-		if (aux->info.nume != NULL) {
-			free(aux->info.nume);
-		}
-		if (aux->info.preturiMedicamente != NULL) {
-			free(aux->info.preturiMedicamente);
-		}
-		free(aux);
-	}
-
-	lista->first = NULL;
-	lista->last = NULL;
-	lista->nrNoduri = 0;
-}
-
-float calculeazaPretMediu(ListaDubla lista) {
-	if(lista.first)
-	{
-		float suma = 0;
-		int nrTotalMedicamente = 0;
-		Nod* p = lista.first;
-		while (p) {
-			for(int i = 0; i<p->info.numarMedicamente;i++)
-			{
-				suma = suma + p->info.preturiMedicamente[i];
-				
-			}
-			p = p->next;
-
-		}
-
-		return suma / nrTotalMedicamente;
-	}
-
-	return 0;
-}
-
-void stergeFarmacieDupaID(/*lista masini*/ int id) {
-	//sterge masina cu id-ul primit.
-	//tratati situatia ca masina se afla si pe prima pozitie, si pe ultima pozitie
-}
-
-char* getNumeSoferMasinaScumpa(/*lista dublu inlantuita*/) {
-	//cauta masina cea mai scumpa si 
-	//returneaza numele soferului acestei maasini.
-	return NULL;
-}
-
-int main() {
-
-
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+////trebuie sa folositi fisierul masini.txt
+////sau va creati un alt fisier cu alte date
+//
+//struct Farmacie {
+//
+//	int id;
+//	char* nume;
+//	unsigned char numarMedicamente;
+//	float* preturiMedicamente;
+//
+//};
+//typedef struct Farmacie Farmacie;
+//
+//
+//struct Nod {
+//
+//	Farmacie info;
+//	struct Nod* prev;
+//	struct Nod* next;
+//
+//};
+//typedef struct Nod Nod;
+//
+//
+//
+//struct ListaDubla {
+//	Nod* first;
+//	Nod* last;
+//	int nrNoduri;
+//};
+//typedef struct ListaDubla ListaDubla;
+//
+//Farmacie citireFarmacieDinFisier(FILE* file) {
+//	Farmacie f = { 0, NULL, 0, NULL };
+//	char buffer[300];
+//	char* separator = ",;\n";
+//
+//	if (fgets(buffer, sizeof(buffer), file) != NULL) {
+//		char* token = strtok(buffer, separator);
+//		if (token == NULL) return f;
+//		f.id = atoi(token);
+//
+//		token = strtok(NULL, separator);
+//		if (token == NULL) return f;
+//		f.nume = (char*)malloc(strlen(token) + 1);
+//		if (f.nume) {
+//			strcpy(f.nume, token);
+//		}
+//
+//		token = strtok(NULL, separator);
+//		if (token == NULL) {
+//			free(f.nume);
+//			f.nume = NULL;
+//			return f;
+//		}
+//		f.numarMedicamente = (unsigned char)atoi(token);
+//
+//		f.preturiMedicamente = (float*)malloc(sizeof(float) * f.numarMedicamente);
+//		if (f.preturiMedicamente == NULL) {
+//			free(f.nume);
+//			f.nume = NULL;
+//			return f;
+//		}
+//
+//		for (int i = 0; i < f.numarMedicamente; i++) {
+//			token = strtok(NULL, separator);
+//			if (token == NULL) {
+//				free(f.nume);
+//				free(f.preturiMedicamente);
+//				f.nume = NULL;
+//				f.preturiMedicamente = NULL;
+//				f.numarMedicamente = 0;
+//				return f;
+//			}
+//			f.preturiMedicamente[i] = (float)atof(token);
+//		}
+//	}
+//
+//	return f;
+//}
+//
+//
+//void afisareFarmacii(Farmacie farmacie) {
+//
+//	printf("ID este : %d\n", farmacie.id);
+//	printf("Numele farmaciei este : %s\n", farmacie.nume);
+//	printf("Numar de medicamente : %d\n", farmacie.numarMedicamente);
+//	printf("Preturile sunt:\n");
+//	for(int i=0;i<farmacie.numarMedicamente;i++)
+//	{
+//		printf(" : %.2f\n", farmacie.preturiMedicamente[i]);
+//	}
+//	printf("\n");
+//	
+//}
+//
+//void afisareListaFarmaciiINCEPUT(ListaDubla lista) {
+//	Farmacie f;
+//	Nod* p = lista.first;
+//	while (p) {
+//
+//		afisareFarmacii(p->info);
+//		p = p->next;
+//	}
+//
+//
+//}
+//
+//void afisareListaFarmaciiSfarsit(ListaDubla lista) {
+//	Farmacie f;
+//	Nod* p = lista.last;
+//	while (p) {
+//
+//		afisareFarmacii(p->info);
+//		p = p->prev;
+//	}
+//
+//
+//}
+//
+//void adaugaFarmacieInListaFINAL(ListaDubla*lista, Farmacie FarmacieNoua) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//	nou->info = FarmacieNoua;
+//	nou->next = NULL;
+//	nou->prev = lista->last;
+//
+//	if (lista->last == NULL) {
+//		lista->first = lista->last = nou;
+//	}
+//	else {
+//		lista->last->next = nou;
+//		nou->prev = lista->last;
+//		lista->last = nou;
+//	}
+//	;
+//	lista->nrNoduri++;
+//
+//}
+//
+//void adaugaLaInceputInLista(ListaDubla* lista, Farmacie FarmacieNoua) {
+//	Nod* nou = (Nod*)malloc(sizeof(Nod));
+//	nou->info = FarmacieNoua;
+//	nou->next = NULL;
+//	nou->prev = lista->first;
+//
+//	if (lista->first == NULL) {
+//		lista->first=lista->last = nou;
+//	}
+//	else {
+//		lista->first->prev = nou;
+//		lista->first = nou;
+//	}
+//
+//	
+//	lista->nrNoduri++;
+//
+//}
+//
+//ListaDubla citireLDFarmacieDinFisier(const char* numeFisier) {
+//	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+//	//prin apelul repetat al functiei citireMasinaDinFisier()
+//	//ATENTIE - la final inchidem fisierul/stream-ul
+//	FILE* f = fopen(numeFisier, "r");
+//	ListaDubla lista;
+//	lista.first = NULL;
+//	lista.last = NULL;
+//	lista.nrNoduri = 0;
+//
+//	if (f == NULL) {
+//		printf("Eroare la deschiderea fisierului %s\n", numeFisier);
+//		return lista;
+//	}
+//
+//	Farmacie farmacie;
+//	while ((farmacie = citireFarmacieDinFisier(f)).nume != NULL) {
+//		adaugaLaInceputInLista(&lista, farmacie);
+//	}
+//
+//	fclose(f);
+//	return lista;
+//}
+//
+//void dezalocareLDFarmacie(ListaDubla* lista) {
+//	Nod* p = lista->first;
+//	while (p) {
+//		Nod* aux = p;
+//		p = p->next;
+//		if (aux->info.nume != NULL) {
+//			free(aux->info.nume);
+//		}
+//		if (aux->info.preturiMedicamente != NULL) {
+//			free(aux->info.preturiMedicamente);
+//		}
+//		free(aux);
+//	}
+//
+//	
+//}
+//
+//float calculeazaPretMediu(ListaDubla lista) {
+//	if(lista.first)
+//	{
+//		float suma = 0;
+//		int nrTotalMedicamente = 0;
+//		Nod* p = lista.first;
+//		while (p) {
+//			for(int i = 0; i<p->info.numarMedicamente;i++)
+//			{
+//				suma = suma + p->info.preturiMedicamente[i];
+//				nrTotalMedicamente++;
+//				
+//			}
+//			p = p->next;
+//
+//		}
+//
+//		return suma / nrTotalMedicamente;
+//	}
+//
+//	return 0;
+//}
+//
+//void stergeFarmacieDupaID(ListaDubla*lista, int id) {
+//	
+//	if (lista->first == NULL) {
+//		return;
+//	}
+//
+//	Nod* p = lista->first;
+//	while (p&&p->info.id != id) {
+//		p = p->next;
+//	}
+//	if (p == NULL) {
+//		return;
+//	}
+//
+//	if (p->prev == NULL) {
+//		lista->first = p->next;
+//		if (lista->first) {
+//			lista->first->prev = NULL;
+//		}
+//
+//	}
+//	else {
+//		p->prev->next = p->next;
+//	}
+//
+//	if (p->next!=NULL) {
+//		p->next->prev = p->prev;
+//
+//	}
+//	else {
+//		lista->last = p->prev;
+//	}
+//
+//	if (p->info.nume != NULL) {
+//		free(p->info.nume);
+//	}
+//	if (p->info.preturiMedicamente != NULL) {
+//		free(p->info.preturiMedicamente);
+//	}
+//	free(p);
+//	p->info.nume = NULL;
+//	p->info.preturiMedicamente = NULL;
+//	lista->nrNoduri--;
+//
+//
+//}
+//
+//char* getNumeSoferMasinaScumpa(ListaDubla lista) {
+//	/*cauta farmacia cu cel mai scump medicament
+//	returneaza numele farmaciei.*/
+//	
+//	if (lista.first == NULL) {
+//		return NULL;
+//	}
+//
+//
+//	Nod* p = lista.first;
+//	Nod* maxFarmacie = NULL;
+//	float maxPret = 0;
+//		while (p) {
+//		for(int i =0;i<p->info.numarMedicamente;i++)
+//		{
+//			if (p->info.preturiMedicamente[i] > maxPret) {
+//				maxPret = p->info.preturiMedicamente[i];
+//				maxFarmacie = p;
+//			}
+//		}
+//
+//			p = p->next;
+//		}
+//
+//		if(maxFarmacie)
+//
+//		{
+//			char* numeF = (char*)malloc(strlen(maxFarmacie->info.nume) + 1);
+//			strcpy(numeF, maxFarmacie->info.nume);
+//			return numeF;
+//		}
+//		else {
+//			return NULL;
+//		}
+//
+//		
+//	
+//
+//	
+//}
+//
+//int main() {
+//
+//	
+//
+//	ListaDubla lista = citireLDFarmacieDinFisier("farmacie.txt");
+//
+//	afisareListaFarmaciiINCEPUT(lista);
+//
+//	return 0;
+//}
--- ListaDublaStudent.c
+++ ListaDublaStudent.c
@@ -1,73 +1,272 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-////trebuie sa folositi fisierul masini.txt
-////sau va creati un alt fisier cu alte date
-//
-//struct Student {
-//	int id;
-//		int varsta;
-//		char* nume;
-//		float medie;
-//		char initialaTata;
-//};
-//typedef struct Student Student;
-//
-////creare structura pentru un nod dintr-o lista dublu inlantuita
-//
-////creare structura pentru Lista Dubla 
-//
-//Student citireMasinaDinFisier(FILE* file) {
-//	
-//}
-//
-//void afisareMasina(Student masina) {
-//	
-//}
-//
-//void afisareListaStudent(/*lista dubla de masini*/) {
-//	//afiseaza toate elemente de tip masina din lista dublu inlantuita
-//	//prin apelarea functiei afisareMasina()
-//}
-//
-//void adaugaStudentInLista(/*lista dubla de masini*/ Student masinaNoua) {
-//	//adauga la final in lista primita o noua masina pe care o primim ca parametru
-//}
-//
-//void adaugaLaInceputInLista(/*lista dubla de masini*/ Student masinaNoua) {
-//	//adauga la inceputul listei dublu inlantuite o noua masina pe care o primim ca parametru
-//}
-//
-//void* citireLDStudentDinFisier(const char* numeFisier) {
-//	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
-//	//prin apelul repetat al functiei citireMasinaDinFisier()
-//	//ATENTIE - la final inchidem fisierul/stream-ul
-//}
-//
-//void dezalocareLDStudent(/*lista dubla de masini*/) {
-//	//sunt dezalocate toate masinile si lista dublu inlantuita de elemente
-//}
-//
-//float calculeazaPretMediu(/*lista de masini*/) {
-//	//calculeaza pretul mediu al masinilor din lista.
-//	return 0;
-//}
-//
-//void stergeStudentDupaID(/*lista masini*/ int id) {
-//	//sterge masina cu id-ul primit.
-//	//tratati situatia ca masina se afla si pe prima pozitie, si pe ultima pozitie
-//}
-//
-//char* getNumeSoferMasinaScumpa(/*lista dublu inlantuita*/) {
-//	//cauta masina cea mai scumpa si 
-//	//returneaza numele soferului acestei maasini.
-//	return NULL;
-//}
-//
-//int main() {
-//
-//
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+struct Student {
+		int id;
+		int varsta;
+		char* nume;
+		float medie;
+		char initialaTata;
+};
+typedef struct Student Student;
+
+struct Nod {
+	Student info;
+	struct Nod* prev;
+	struct Nod* next;
+
+};
+typedef struct Nod Nod;
+
+struct ListDubla {
+	Nod* first;
+	Nod* last;
+	int nrNoduri;
+};
+
+Student citireStudentDinFisier(FILE* file) {
+	Student s = { 0 };
+	char buffer[100];
+	char* separator = " ,;\n";
+	fgets(buffer, 100, file);
+
+	char* token = strtok(buffer, separator);
+	if (token==NULL) return s;
+	s.id = atoi(token);
+
+	token = strtok(NULL, separator);
+	if (token == NULL) return s;
+	s.varsta = atoi(token);
+	
+	token = strtok(NULL, separator);
+	if (token == NULL) return s;
+	s.nume = (char*)malloc(strlen(token) + 1);
+	strcpy(s.nume, token);
+	
+	token = strtok(NULL, separator);
+	if (token == NULL) return s;
+	s.medie = atof(token);
+	
+	token = strtok(NULL, separator);
+	if (token == NULL) return s;
+	s.initialaTata = (token)[0];
+	
+	return s;
+
+	
+}
+
+void afisareStudent(Student student) {
+	
+	printf("ID:%d\n", student.id);
+	printf("Varsta:%d\n", student.varsta);
+	printf("Nume:%s\n", student.nume);
+	printf("Medie:%.2f\n", student.medie);
+	printf("InitialaTata:%c\n", student.initialaTata);
+	printf("\n");
+}
+
+Student initalizare(int id,int varsta,const char* nume,float medie,char initialaTata) {
+	Student s;
+	s.id = id;
+	s.varsta = varsta;
+	s.nume = (char*)malloc(strlen(nume) + 1);
+	strcpy(s.nume, nume);
+	s.medie = medie;
+	s.initialaTata = initialaTata;
+	return s;
+
+}
+
+void dezalocareStudent(Student* s) {
+
+	if (s->nume != NULL) {
+		free(s->nume);
+		s->nume = NULL;
+	}
+	
+}
+
+void afisareVector(Student* s, int nrStudenti) {
+
+	for (int i = 0; i < nrStudenti; i++) {
+		afisareStudent(s[i]);
+	}
+
+}
+
+void dezalocareVector(Student** s, int* nrStudenti) {
+
+	for (int i = 0; i < (*nrStudenti); i++) {
+		if ((*s)[i].nume != NULL) {
+			free((*s)[i].nume);
+		}
+	}
+
+	free(*s);
+	(*s) = NULL;
+	*nrStudenti = 0;
+
+}
+
+ Student* copiazaPrimeleNElemente( Student* vector, int nrElemente, int nrElementeCopiate) {
+	
+	Student *vectorNou=NULL;
+	vectorNou = (Student*)malloc(sizeof(Student) * nrElementeCopiate);
+	for (int i = 0; i < nrElementeCopiate; i++) {
+		vectorNou[i].id = vector[i].id;
+		vectorNou[i].varsta = vector[i].varsta;
+		vectorNou[i].nume = (char*)malloc(strlen(vector[i].nume) + 1);
+		strcpy(vectorNou[i].nume, vector[i].nume);
+
+		vectorNou[i].medie = vector[i].medie;
+		vectorNou[i].initialaTata = vector[i].initialaTata;
+	}
+
+
+
+	return vectorNou;
+}
+
+
+ void copiazaAnumiteElemente(Student* vector, char nrElemente, float prag,  Student** vectorNou, int* dimensiune) {
+
+	 *dimensiune = 0;
+	 for (int i = 0; i < nrElemente; i++) {
+		 if (vector[i].medie > prag) {
+			 (*dimensiune)++;
+		 }
+	 }
+
+	 if (*vectorNou != NULL) {
+		 free(*vectorNou);
+	 }
+
+	 *vectorNou = (Student*)malloc(sizeof(Student) * (*dimensiune));
+	 int k = 0;
+	 for (int i = 0; i < nrElemente; i++) {
+		
+		 if(vector[i].medie>prag)
+		 {
+			 (*vectorNou)[k] = vector[i];
+			 (*vectorNou)[k].nume = (char*)malloc(strlen(vector[i].nume)+1);
+			 strcpy((*vectorNou)[k].nume, vector[i].nume);
+			 k++;
+		 }
+
+
+	 }
+
+
+}
+
+ Student getPrimulElementConditionat(Student* vector, int nrElemente, const char* numeCautat){
+	
+	Student s = {0};
+	for (int i = 0; i < nrElemente; i++) {
+		if (strcmp(vector[i].nume, numeCautat) == 0) {
+			return vector[i];
+		}
+		
+		
+	}
+
+	return s;
+	
+	
+}
+
+
+void afisareListaStudent(/*lista dubla de masini*/) {
+	//afiseaza toate elemente de tip masina din lista dublu inlantuita
+	//prin apelarea functiei afisareMasina()
+}
+
+void adaugaStudentInLista(/*lista dubla de masini*/ Student masinaNoua) {
+	//adauga la final in lista primita o noua masina pe care o primim ca parametru
+}
+
+void adaugaLaInceputInLista(/*lista dubla de masini*/ Student masinaNoua) {
+	//adauga la inceputul listei dublu inlantuite o noua masina pe care o primim ca parametru
+}
+
+void* citireLDStudentDinFisier(const char* numeFisier) {
+	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+	//prin apelul repetat al functiei citireMasinaDinFisier()
+	//ATENTIE - la final inchidem fisierul/stream-ul
+}
+
+void dezalocareLDStudent(/*lista dubla de masini*/) {
+	//sunt dezalocate toate masinile si lista dublu inlantuita de elemente
+}
+
+float calculeazaPretMediu(/*lista de masini*/) {
+	//calculeaza pretul mediu al masinilor din lista.
+	return 0;
+}
+
+void stergeStudentDupaID(/*lista masini*/ int id) {
+	//sterge masina cu id-ul primit.
+	//tratati situatia ca masina se afla si pe prima pozitie, si pe ultima pozitie
+}
+
+char* getNumeSoferMasinaScumpa(/*lista dublu inlantuita*/) {
+	//cauta masina cea mai scumpa si 
+	//returneaza numele soferului acestei maasini.
+	return NULL;
+}
+
+int main() {
+
+	
+	/*Student s1 = initalizare(1, 20, "Popescu", 9.5, 'I');
+	Student s2 = initalizare(1, 21, "Ionescu", 8.5, 'V');
+
+	printf("----S1------\n");
+	afisareStudent(s1);
+	printf("----S2------\n");
+	afisareStudent(s2);
+	dezalocareStudent(&s1);
+	dezalocareStudent(&s2);
+
+	printf("----S3 din fisier------\n");
+	FILE* f = fopen("student.txt", "r");
+	Student s3= citireStudentDinFisier(f);
+	afisareStudent(s3);
+	dezalocareStudent(&s3);
+	fclose(f);*/
+
+	int nrStudenti = 4;
+	Student* vector = (Student*)malloc(sizeof(Student)*nrStudenti);
+	vector[0] = initalizare(1, 20, "Popescu", 9.5, 'I');
+	vector[1] = initalizare(2, 21, "Ionescu", 8.5, 'V');
+	vector[2] = initalizare(3, 19, "Vasilescu", 6.5, 'C');
+	vector[3] = initalizare(4, 25, "Popescu", 9.5, 'I');
+	
+	afisareVector(vector, nrStudenti);
+
+	int nrElementeCopiate = 2;
+	Student* primeleElemente = NULL;
+	primeleElemente = copiazaPrimeleNElemente(vector, nrStudenti, nrElementeCopiate);
+	printf("----Vector Copiat----\n");
+	afisareVector(primeleElemente, nrElementeCopiate);
+
+	Student* anumitiStudenti = NULL;
+	int nrAnumitiStudenti = 0; 
+	copiazaAnumiteElemente(vector, nrStudenti, 9.0, &anumitiStudenti, &nrAnumitiStudenti);
+	printf("----Vector Copiat----\n");
+	afisareVector(anumitiStudenti,nrAnumitiStudenti);
+
+	Student rezultat = getPrimulElementConditionat(vector, nrStudenti, "Popescu");
+	afisareStudent(rezultat);
+
+	dezalocareVector(&anumitiStudenti, &nrAnumitiStudenti);
+	dezalocareVector(&vector, &nrStudenti);
+	dezalocareVector(&primeleElemente, &nrElementeCopiate);
+	
+	return 0;
+}
